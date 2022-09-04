18d0618e71622f6b6beaaeaba7a9c2c05275291c
Tomas Mendes Diz <39938773+tmendesdiz@users.noreply.github.com>
2022-09-04 16:39:49 -0300

    Update II._datos_de_interes.py

--- presentacion/pages/II._datos_de_interes.py
+++ presentacion/pages/II._datos_de_interes.py
@@ -1,3 +1,14 @@
+'''
+Este es el script de la pestaña en la que se muestran los graficos relacionados al dataset de los vehiculos involucrados en los accidentes.
+Aca mismo se definen funciones para llamar a la api de los datos simplificados y se los usa para graficar la información de interes.
+
+Las librerias que se usan son streamlit, pandas, plotly.expres y requests.
+Los requests realizados son a la api: http://vps-2671696-x.dattaweb.com/swagger/
+
+Dentro de cada función se detalla su utilidad y los parametreos que reciben. Este es el listado de las mismas:
+grafico_para_factores, grafico_licencias_estados, crear_df_danio, grafico_daño_propiedad.
+'''
+
 import streamlit as st
 import pandas as pd
 import plotly.express as px
@@ -13,6 +24,17 @@ df_factores_clean = df_factores.loc[df_factores['contributing_factor'] != 'Unspe
 df_factores_clean.head()
 
 def grafico_para_factores(df, anio, sexo):
+  '''
+  Parametros:
+    df = Es el dataframe que recibe para realizar el grafico correspondiente. 
+      Deberia pasarse el dataframe sacado de la variable url_factores ya que cumple con los features que utiliza la función.
+    anio = Limita el dataframe a los que corresponden a ese año.
+    sexo = Es un parametro que permite limitar el grafico por el parametro sexo del dataframe.
+    
+  Utilidad:
+    Esta funcion, con los parametros anteriores, realiza un grafico que representa el top 5 de los factores que contribuyen a los accidentes.
+    El parametro sexo quedo fijado para que la funcion represente a ambos pero se puede utilizar para realizar la distinción.
+  '''
   df = df.loc[df['crash_year'] == anio]
   data_m = df.loc[df['driver_sex'] == 'Male'].sort_values('qty', ascending=False)
   data_f = df.loc[df['driver_sex'] == 'Female'].sort_values('qty', ascending=False)
@@ -35,6 +57,18 @@ df_licences = pd.DataFrame(response_licences.json()['data'])
 df_licences_clean = df_licences.loc[df_licences['driver_license_jurisdiction'] != 'Unknown']
 
 def grafico_licencias_estados(df, anio):
+  '''
+  Parametros:
+    df = Es el dataframe que recibe para realizar el grafico correspondiente. 
+      Deberia pasarse el dataframe sacado de la variable url_licences ya que cumple con los features que utiliza la función.
+    anio = Limita el dataframe a los que corresponden a ese año.
+    
+  Utilidad:
+    Esta funcion, con los parametros anteriores, realiza un grafico que representa el top 5 de los Estados 
+    por los que fueron emitidas las licencias de los conductores que participaron en los accidentes. 
+    Para esto limita el df segun el parametro anio y excluye las licencias de Nueva York, luego las ordena de forma descendente 
+    y realiza un grafico de barras con las 5 mayores.
+  '''
   df = df.loc[(df['year'] == anio)&(df['driver_license_jurisdiction'] != 'NY')]
   df = df.sort_values('qty', ascending=False)
   fig = px.bar(df.head(), x = 'driver_license_jurisdiction', y='qty', color='driver_license_jurisdiction')
@@ -44,12 +78,30 @@ def grafico_licencias_estados(df, anio):
 
 # Funcion para crear df (hago una funcion porque hay q pasarle el año a la url)
 def crear_df_danio(anio):
+  '''
+  Parametros:
+    anio = Limita el dataframe a los que corresponden a ese año.
+    
+  Utilidad:
+    Esta funcion crea un dataframe en determinado año llamando a la url de la api con los datos simplificados.
+  '''
   url_damage = 'http://vps-2671696-x.dattaweb.com/collision/public_damage/'+ str(anio)
   response_damage = requests.get(url_damage)
   df_damage = pd.DataFrame(response_damage.json()['data'])
   return df_damage
+
+
 # Funcion para graficar (usa la funcion anterior)
 def grafico_daño_propiedad(anio):
+  '''
+  Parametros:
+    anio = Limita el dataframe a los que corresponden a ese año.
+    
+  Utilidad:
+    Esta funcion, llama a la funcion 'crear_df_danio' y le pasa el parametro que recibe para crear un dataframe en ese año particular.
+    Luego ordena los horarios en los que mayor daño a la propiedad se causó, para luego realizar un grafico de barras.
+    Para esto limita el df segun el parametro anio y excluye las licencias de Nueva York, luego las ordena de forma descendente y grafica con las 5 mayores.
+  '''
   df = crear_df_danio(anio)
   df = df.sort_values('qty', ascending=False).head()
   fig = px.bar(df, x = 'crash_time', y='qty', color='crash_time')
